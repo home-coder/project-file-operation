50bbeb79705591b31234a9fbf9e20bc86fe9b808
oneface <jiangxj@xiaolj.com>
2018-05-22 17:00:12 +0800

    base update

---
+++ 001.c
@@ -0,0 +1,91 @@
+/***************************************************************************
+ > Filename   : 001.c
+ > Author     : oneface - one_face@sina.com
+ > Company    : 一尊还酹江月
+ > Time       : 2018-05-21 17:30:16
+ > Description: 
+
+ - This program is free software; you can redistribute it and/or modify it
+ - under the terms of the GNU General Public License as published by the
+ - Free Software Foundation;  either version 2 of the  License, or (at your
+ - option) any later version.
+ **************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+#define TESTFILE	"./test"
+#define TESTFILE2	"./test2"
+#define TESTFILE3	"./test3"
+
+#define BUF_SIZE	32
+
+//1. 文本文件和二进制文件都可以在屏幕上显示，但是二进制文件的内容无法读懂，大部分是乱码。
+//   文本文件是一种特殊的二进制文件，在于每个字符流的字符可以显示出ascci码
+
+//2. 在Linux和其他一些系统中，文本文件的换行符就是LF(0X0A)，与C语言的换行符一样。所以也就沒有了文本方式和二进制方式的区分，使不使用'b'标志都是一样的。
+
+int main()
+{
+	//输出到文件中，然后通过vim 打开，再转换成文本查看 是不是我们输入的字符
+#if 0
+	FILE *fp = fopen(TESTFILE, "wb");
+
+	char buf[5] = { 0x61, 0x62, 0x63, 0x01, 0x32 };
+	fwrite(buf, sizeof(buf[0]), 5, fp);
+	fclose(fp);
+
+	fp = fopen(TESTFILE, "rb");
+	FILE *fp2 = fopen(TESTFILE2, "w");
+	int num = fread(buf, sizeof(char), BUF_SIZE, fp);
+	while (num > 0) {
+		fwrite(buf, sizeof(char), num, fp2);
+		num = fread(buf, 1, BUF_SIZE, fp);
+	}
+
+	fclose(fp);
+	fclose(fp2);
+#endif
+
+#if 0
+	FILE *fp = fopen("001.c", "rb");
+	FILE *fp2 = fopen("001bac.c", "wb"); //linux 平台下可以w，但是window下如果没有b，则默认成普通文本文件会将0a替换成0d0a
+	int num;
+	char buf[BUF_SIZE];
+
+	while ((num = fread(buf, 1, BUF_SIZE, fp)) > 0) {
+		fwrite(buf, num, BUF_SIZE, fp);
+	}
+#endif
+
+#if 1 //0x0A 是换行还是文件结束符
+	FILE *fp = fopen(TESTFILE3, "wb");
+	char buf[] = {0x61, 0xff, 0x62, 0x63, 0x01, 0x32, 0x0a, 0x66, 0xff, 0x62, 0x64, 0x0a, 0x67, 0x68};
+	fwrite(buf, sizeof(buf[0]), sizeof(buf)/sizeof(buf[0]), fp);
+	fclose(fp);
+	fp = fopen(TESTFILE3, "r");
+	char c;
+#if 1 //文本文件可以通过EOF来判断，EOF=0xff=-1, 不在0-127  Ascci码范围，所以可以使用EOF， 但是二进制则不可以
+	while ((c = fgetc(fp)) != EOF) {
+		printf("%c ", c);
+	}
+#endif
+
+//TODO
+#if 0 // 0xff 是可以通过feof判断吗， feof判断的是什么东西, 为什么文本文件和二进制都可以使用feof
+	c = fgetc(fp);
+	while (!feof(fp)) {
+		printf("%c ", c);
+		c = fgetc(fp);
+	}
+#endif
+
+	//TODO: 8765的存储，以二进制存储，以文本存储
+	//TODO: 8765的存储，以二进制存储fwrite，以文本存储fprintf
+#if 0
+#endif
+
+	fclose(fp);
+#endif
+	return 0;
+
+}
--- README.md
+++ README.md
@@ -1,22 +1,40 @@
-# struct-senior-fileopt
-将结构体仓howto-use-struct的基本任务完成后，完成此项目，需求需要后期更新README.md
-
+# File operations
 
 项目描述
 ------------------------------------------------------------------------------------------------------
-利用struct stat， readdir, 等等Unix高级环境编程里面的函数来实现 find, ls, grep, cat, touch,等
+#### 1.强化字符  字符串  文本  二进制  fopen系列 open系列 移位操作  等重复练习
+* 1.1 二进制的练习  
+* 1.2 字符串与二进制结合的练习  
+* 1.3 文本文件操作+二进制文件操作
+* 1.4 所有相关函数族的练习：fopen ftell rewind  fprintf sprintf strcat strstr isalpha等等
 
+### Q&A  
+* 1.验证： 二进制读写是将内存里面的数据直接读写入文本中，而文本呢，则是将数据先转换成了字符串，再写入到文本中。下面我用个例子来说明。
+我们定义了一个结构体，表示一个学生信息，我们打算把学生的信息分别用二进制和文本的方式写入到文件中。
+
+* 2.fprintf  fwrite对写入的处理, 如果不清楚看下面的参考链接
+
+### 总结
+* 写一篇总结性的文章《论文本操作》
+参考：  
+[二进制方式和文本方式的区别](https://blog.csdn.net/renjiewen1995/article/details/52108645)
+[fprintf与fwrite使用区别](https://blog.csdn.net/godenlove007/article/details/7721647)
+#### 2. 利用struct stat， readdir, 等等Unix高级环境编程里面的函数来实现 find, ls, grep, cat, touch,等
 执行方式 shell# ./a.out -find file.txt
+接口函数如下  
+** yang_find_file(filename);**   
+** yang_ls_file();**  
+** yang_grep_string(strname);**  
+** yang_cat_file(filename);**  
+** yang_touch_file(filename);**
 
-接口函数如下
-*yang_find_file(filename);
-**yang_ls_file();
-**yang_grep_string(strname);
-**yang_cat_file(filename);
-**yang_touch_file(filename);
+---
+## 特殊说明
+ls等支持参数，如 ./a.out -ls -l  , grep -nIr hello  
 
-*特殊说明
-ls等支持参数，如 ./a.out -ls -l  , grep -nIr hello
+ My 编程珠玑：
+ > 根据编程网站的调查分析，所有工程代码统计的结果表明：30%的工作量是字符的操作，所以字符相关的操作一定要手到擒来
 
-编程注意，目的是强化结构体的熟识度，所以要把struct stat等内核结构体直接写到代码里面，最好不用include包含,目的是方便看着操作
------------------------------------------------------------------------------------------------------------
+---
+# &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 《论文本操作》  
+xxx
