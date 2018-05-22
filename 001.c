12acf346103a66d691b74292cf71ecdbc9df14be
oneface <jiangxj@xiaolj.com>
2018-05-22 19:06:43 +0800

    几个添加和总结

--- 001.c
+++ 001.c
@@ -1,17 +1,19 @@
 /***************************************************************************
- > Filename   : 001.c
- > Author     : oneface - one_face@sina.com
- > Company    : 一尊还酹江月
- > Time       : 2018-05-21 17:30:16
- > Description: 
+  > Filename   : 001.c
+  > Author     : oneface - one_face@sina.com
+  > Company    : 一尊还酹江月
+  > Time       : 2018-05-21 17:30:16
+  > Description: 
 
- - This program is free software; you can redistribute it and/or modify it
- - under the terms of the GNU General Public License as published by the
- - Free Software Foundation;  either version 2 of the  License, or (at your
- - option) any later version.
+  - This program is free software; you can redistribute it and/or modify it
+  - under the terms of the GNU General Public License as published by the
+  - Free Software Foundation;  either version 2 of the  License, or (at your
+  - option) any later version.
  **************************************************************************/
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <sys/stat.h>
 
 #define TESTFILE	"./test"
 #define TESTFILE2	"./test2"
@@ -30,7 +32,7 @@ int main()
 #if 0
 	FILE *fp = fopen(TESTFILE, "wb");
 
-	char buf[5] = { 0x61, 0x62, 0x63, 0x01, 0x32 };
+	uint8_t buf[5] = { 0x61, 0x62, 0x63, 0x01, 0x32 };
 	fwrite(buf, sizeof(buf[0]), 5, fp);
 	fclose(fp);
 
@@ -57,20 +59,22 @@ int main()
 	}
 #endif
 
-#if 1 //0x0A 是换行还是文件结束符
+#if 0 //0x0A 是换行还是文件结束符
+
 	FILE *fp = fopen(TESTFILE3, "wb");
-	char buf[] = {0x61, 0xff, 0x62, 0x63, 0x01, 0x32, 0x0a, 0x66, 0xff, 0x62, 0x64, 0x0a, 0x67, 0x68};
+	//比较有符号和无符号的区别
+	uint8_t buf[] = {0x61, 0x62, 0x7e, 0x63, 0xff, 0x63, 0x01, 0x32, 0x0a, 0x66, 0xff, 0x62, 0x64, 0x0a, 0x67, 0x68};
 	fwrite(buf, sizeof(buf[0]), sizeof(buf)/sizeof(buf[0]), fp);
 	fclose(fp);
 	fp = fopen(TESTFILE3, "r");
 	char c;
-#if 1 //文本文件可以通过EOF来判断，EOF=0xff=-1, 不在0-127  Ascci码范围，所以可以使用EOF， 但是二进制则不可以
+#if 0 //文本文件可以通过EOF来判断，EOF=0xff=-1, 不在0-127  Ascci码范围，所以可以使用EOF， 但是二进制则不可以
 	while ((c = fgetc(fp)) != EOF) {
 		printf("%c ", c);
 	}
 #endif
 
-//TODO
+	//TODO
 #if 0 // 0xff 是可以通过feof判断吗， feof判断的是什么东西, 为什么文本文件和二进制都可以使用feof
 	c = fgetc(fp);
 	while (!feof(fp)) {
@@ -79,13 +83,39 @@ int main()
 	}
 #endif
 
-	//TODO: 8765的存储，以二进制存储，以文本存储
-	//TODO: 8765的存储，以二进制存储fwrite，以文本存储fprintf
+	//比较有符号和无符号的区别
+#if 1
+	int num;
+	for (num = 0; num < sizeof(buf)/sizeof(buf[0]); num++) {
+		printf("0x%x ", buf[num]);	
+	}
+	while ((num = fread(buf, sizeof(char), 2, fp))) {
+		printf("0x%x ", buf[0]);	
+	}
+#endif
 #if 0
 #endif
-
 	fclose(fp);
 #endif
-	return 0;
+#if 1
+	//TODO: 8765的存储，以二进制存储，以文本存储
+	int a = 8765999;
+#if 0
+	FILE *fp = fopen("8765", "w+b");
+	fwrite(&a, sizeof(int), 1, fp);
+#else 
+	FILE *fp = fopen("8765", "w+");
+	fprintf(fp, "%d", a);
+#endif
+	fclose(fp);
+	unsigned long filesize = -1;      
+	struct stat statbuff;  
+	if(stat("8765", &statbuff) >= 0){  
+		filesize = statbuff.st_size;  
+	}
+	printf("8765 size: %ld\n", filesize);
+#endif
+	//TODO: 8765的存储，以二进制存储fwrite，以文本存储fprintf
 
+	return 0;
 }
--- README.md
+++ README.md
@@ -12,13 +12,17 @@
 * 1.验证： 二进制读写是将内存里面的数据直接读写入文本中，而文本呢，则是将数据先转换成了字符串，再写入到文本中。下面我用个例子来说明。
 我们定义了一个结构体，表示一个学生信息，我们打算把学生的信息分别用二进制和文本的方式写入到文件中。
 
-* 2.fprintf  fwrite对写入的处理, 如果不清楚看下面的参考链接
+* 2.fprintf  fwrite对写入的处理, 如果不清楚看下面的**参考链接**
 
 ### 总结
-* 写一篇总结性的文章《论文本操作》
-参考：  
+* 写一篇总结性的文章《论文本操作》  
+##### **参考**：  
 [二进制方式和文本方式的区别](https://blog.csdn.net/renjiewen1995/article/details/52108645)
-[fprintf与fwrite使用区别](https://blog.csdn.net/godenlove007/article/details/7721647)
+[fprintf与fwrite使用区别](https://blog.csdn.net/godenlove007/article/details/7721647)  
+
+### 实践项目  
+c实现文本编辑器, 参考代码&nbsp;&nbsp;&nbsp;&nbsp;"edit.c"
+
 #### 2. 利用struct stat， readdir, 等等Unix高级环境编程里面的函数来实现 find, ls, grep, cat, touch,等
 执行方式 shell# ./a.out -find file.txt
 接口函数如下  
@@ -38,3 +42,33 @@ ls等支持参数，如 ./a.out -ls -l  , grep -nIr hello
 ---
 # &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 《论文本操作》  
 xxx
+文本文件是二进制文件的一种特殊形式，特殊在对应的每个字节可以转换成0-127内规定的ASCCI码值。
+
+为了兼容win和linux平台，最好将文件打开方式一律规定为 wb， 如果仅仅是w 文本方式，window 平台会将linux平台下的0x0A默认转换成0x0D 0x0A, 这样就将文件变大了. 0x0A对应的是换行符\n, 同理linux打开window文件会出现 ^M是因为她对应了\n\r后面这个\r.
+
+下面写一个标准的打开写入操作，c系统是操作字符流的：
+
+```  
+char buf[BUFSIZE];
+fp = fopen("test", "rb");  
+//if err
+while ((num = fread(buf, sizeof(char), BUFSIZE, fp)) > 0) {
+  fwrite(buf, sizeof(char), num, fp2);
+}
+```
+
+一个标准的读取字符过程(适用于文本或二进制，判断EOF只用于文本)：
+```
+char c = fgetc(fp);
+while (!feof(fp)) {
+    printf("0x%x ", c);
+    c = fgetc(fp);
+}
+```
+有时见到的EOF解释如下：EOF=-1=0xff， 不再0-127ASCCI码值范围，所以被用来判断是否是结尾或出错等.
+//文本的读取
+```
+while ((c = fgec(fp)) != EOF) {
+  printf("%c ", c);
+}
+```
